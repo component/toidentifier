e0440abde63911d78a8d36e01d467b7e31a215bf
Douglas Christopher Wilson <doug@somethingdoug.com>
2018-05-20 00:01:20 -0400

    tests: add more tests for functionality

--- test/index.js
+++ test/index.js
@@ -3,7 +3,19 @@ var assert = require('assert')
 var toIdentifier = require('..')
 
 describe('toIdentifier', function () {
-  it('should transform "Bad Request" to "BadRequest"', function () {
-    assert.equal(toIdentifier('Bad Request', 'BadRequest'), 'BadRequest')
+  it('should remove spaces', function () {
+    assert.equal(toIdentifier('  Foo  Bar  '), 'FooBar')
+  })
+
+  it('should upper case first letter', function () {
+    assert.equal(toIdentifier('foo bar'), 'FooBar')
+  })
+
+  it('should preserve case for ', function () {
+    assert.equal(toIdentifier('foo bar'), 'FooBar')
+  })
+
+  it('should remove non-word characters', function () {
+    assert.equal(toIdentifier('f"o^o $ bar_z'), 'FooBar_z')
   })
 })
