f7484c0d47e5fcb211f5d7e72f64b322e7a5ac9f
Douglas Christopher Wilson <doug@somethingdoug.com>
2019-07-06 00:43:31 -0400

    lint: apply standard 12 style

--- test/index.js
+++ test/index.js
@@ -4,18 +4,18 @@ var toIdentifier = require('..')
 
 describe('toIdentifier', function () {
   it('should remove spaces', function () {
-    assert.equal(toIdentifier('  Foo  Bar  '), 'FooBar')
+    assert.strictEqual(toIdentifier('  Foo  Bar  '), 'FooBar')
   })
 
   it('should upper case first letter', function () {
-    assert.equal(toIdentifier('foo bar'), 'FooBar')
+    assert.strictEqual(toIdentifier('foo bar'), 'FooBar')
   })
 
   it('should preserve case for ', function () {
-    assert.equal(toIdentifier('foo bar'), 'FooBar')
+    assert.strictEqual(toIdentifier('foo bar'), 'FooBar')
   })
 
   it('should remove non-word characters', function () {
-    assert.equal(toIdentifier('f"o^o $ bar_z'), 'FooBar_z')
+    assert.strictEqual(toIdentifier('f"o^o $ bar_z'), 'FooBar_z')
   })
 })
